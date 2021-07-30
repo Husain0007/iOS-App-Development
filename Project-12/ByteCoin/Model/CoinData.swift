57b117c0376ce8e2e3e4cb53fced4a3a30d9a324
Syed Husain Mustafa <syedhusainmustafa@gmail.com>
2021-07-30 13:15:28 +0530

    Bitcoin Conversation Rate App

---
+++ Project-12/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Husain Mustafa on 15/06/21.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData : Decodable
+{
+    let rate : Double
+}
